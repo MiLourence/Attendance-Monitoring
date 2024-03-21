6e29a8242f749adca532792bc8189bc6208332db
MiLourence <160299670+MiLourence@users.noreply.github.com>
2024-03-21 13:15:33 +0800

    test

--- Attendance Monitoring/Menu.Designer.cs
+++ Attendance Monitoring/Menu.Designer.cs
@@ -32,11 +32,14 @@
             this.panelBack = new System.Windows.Forms.Panel();
             this.button6 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button11 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.mainpan = new System.Windows.Forms.Panel();
-            this.button11 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.panelBack.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.mainpan.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -44,10 +47,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(4, 6);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(5, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(221, 27);
+            this.label1.Size = new System.Drawing.Size(279, 35);
             this.label1.TabIndex = 1;
             this.label1.Text = "Attendance Monitoring";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -59,9 +61,9 @@
             this.panelBack.Controls.Add(this.button6);
             this.panelBack.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelBack.Location = new System.Drawing.Point(0, 0);
-            this.panelBack.Margin = new System.Windows.Forms.Padding(2);
+            this.panelBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelBack.Name = "panelBack";
-            this.panelBack.Size = new System.Drawing.Size(1119, 42);
+            this.panelBack.Size = new System.Drawing.Size(1492, 52);
             this.panelBack.TabIndex = 1;
             this.panelBack.Paint += new System.Windows.Forms.PaintEventHandler(this.panelBack_Paint);
             // 
@@ -72,10 +74,10 @@
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button6.ForeColor = System.Drawing.Color.White;
-            this.button6.Location = new System.Drawing.Point(834, 10);
-            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Location = new System.Drawing.Point(1112, 12);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(65, 25);
+            this.button6.Size = new System.Drawing.Size(87, 31);
             this.button6.TabIndex = 3;
             this.button6.Text = "Log Out";
             this.button6.UseVisualStyleBackColor = false;
@@ -87,10 +89,25 @@
             this.panel2.Controls.Add(this.button11);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 42);
+            this.panel2.Location = new System.Drawing.Point(0, 52);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(192, 507);
+            this.panel2.Size = new System.Drawing.Size(256, 624);
             this.panel2.TabIndex = 0;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.White;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Font = new System.Drawing.Font("Sitka Banner", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.Color.Black;
+            this.button11.Location = new System.Drawing.Point(4, 105);
+            this.button11.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(247, 50);
+            this.button11.TabIndex = 7;
+            this.button11.Text = "Print";
+            this.button11.UseVisualStyleBackColor = false;
             // 
             // button1
             // 
@@ -98,9 +115,10 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(3, 18);
+            this.button1.Location = new System.Drawing.Point(4, 22);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(165, 40);
+            this.button1.Size = new System.Drawing.Size(220, 49);
             this.button1.TabIndex = 0;
             this.button1.Text = "Attendance report";
             this.button1.UseVisualStyleBackColor = false;
@@ -109,36 +127,43 @@
             // mainpan
             // 
             this.mainpan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.mainpan.Controls.Add(this.button3);
+            this.mainpan.Controls.Add(this.button2);
             this.mainpan.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainpan.Location = new System.Drawing.Point(192, 42);
+            this.mainpan.Location = new System.Drawing.Point(256, 52);
+            this.mainpan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mainpan.Name = "mainpan";
-            this.mainpan.Size = new System.Drawing.Size(927, 507);
+            this.mainpan.Size = new System.Drawing.Size(1236, 624);
             this.mainpan.TabIndex = 2;
             // 
-            // button11
+            // button2
             // 
-            this.button11.BackColor = System.Drawing.Color.White;
-            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button11.Font = new System.Drawing.Font("Sitka Banner", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button11.ForeColor = System.Drawing.Color.Black;
-            this.button11.Location = new System.Drawing.Point(3, 85);
-            this.button11.Margin = new System.Windows.Forms.Padding(2);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(185, 41);
-            this.button11.TabIndex = 7;
-            this.button11.Text = "Print";
-            this.button11.UseVisualStyleBackColor = false;
+            this.button2.Location = new System.Drawing.Point(337, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(403, 251);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(284, 52);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1119, 549);
+            this.ClientSize = new System.Drawing.Size(1492, 676);
             this.Controls.Add(this.mainpan);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panelBack);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
@@ -146,6 +171,7 @@
             this.panelBack.ResumeLayout(false);
             this.panelBack.PerformLayout();
             this.panel2.ResumeLayout(false);
+            this.mainpan.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -158,5 +184,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel mainpan;
         private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
